6de79316859b10740b70aedfdebf20915b7183a1
Albert Vaillon <albert.vaillon21@gmail.com>
2026-03-03 22:55:52 +0100

    fix: remove loaders

--- src/routes/_mainLayout/space/$spaceId/room/$roomId/index.tsx
+++ src/routes/_mainLayout/space/$spaceId/room/$roomId/index.tsx
@@ -2,23 +2,5 @@ import { Room } from "@/pages/room";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_mainLayout/space/$spaceId/room/$roomId/")({
-    loader: async ({ context, params }) => {
-        const { spaceId, roomId } = params;
-
-        const queries = [
-            context.queryClient.ensureQueryData({
-                queryKey: ["spaces", spaceId],
-                queryFn: () => context.matrixClientContext.client.getRoom(spaceId),
-                staleTime: Infinity
-            }),
-            context.queryClient.ensureQueryData({
-                queryKey: ["spaces", spaceId, "room", roomId],
-                queryFn: () => context.matrixClientContext.client.getRoom(roomId),
-                staleTime: Infinity
-            })
-        ];
-
-        await Promise.all(queries);
-    },
     component: Room
 });
