78375c34c13bc02eb3f3ce29d1b0af0a673b7c35
Albert Vaillon <albert.vaillon21@gmail.com>
2026-03-03 12:29:28 +0100

    feat: remove useless space page

--- src/routes/_mainLayout/space/$spaceId/index.tsx
+++ src/routes/_mainLayout/space/$spaceId/index.tsx
@@ -1,15 +1,8 @@
-import { Space } from "@/pages/space";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_mainLayout/space/$spaceId/")({
-    loader: async ({ context, params }) => {
-        const { spaceId } = params;
-
-        await context.queryClient.ensureQueryData({
-            queryKey: ["spaces", spaceId],
-            queryFn: () => context.matrixClientContext.client.getRoom(spaceId),
-            staleTime: Infinity
-        });
+    beforeLoad: () => {
+        // TODO: redirect to first room
     },
-    component: Space
+    component: () => null
 });
