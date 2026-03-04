32625d7f885228abe0a16d2e4f85ad8b4df981f9
Albert Vaillon <80990528+Eviive@users.noreply.github.com>
2026-03-04 14:40:01 +0100

    Apply suggestion from @Eviive

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,4 +1,4 @@
-import { Login } from "@/pages/login.tsx";
+import { Login } from "@/pages/login";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
