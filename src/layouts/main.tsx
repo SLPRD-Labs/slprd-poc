0f7adf75724bfdf5b6be50c66534294825c1a32c
Albert Vaillon <albert.vaillon21@gmail.com>
2026-03-04 19:42:54 +0100

    feat: close room sidebar when no space is selected

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,18 +1,48 @@
 import { AppSidebar } from "@/components/common/sidebar/sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { CallContextProvider } from "@/contexts/call-context/call-context-provider";
-import { Outlet } from "@tanstack/react-router";
+import { Outlet, useMatchRoute } from "@tanstack/react-router";
 import type { CSSProperties, FC } from "react";
 
+const useSpaceRoomMatchRoute = (): false | { spaceId: string; roomId?: string } => {
+    "use no memo";
+
+    const matchRoute = useMatchRoute();
+
+    const spaceRoomMatchRoute = matchRoute({
+        to: "/space/$spaceId/room/$roomId",
+        fuzzy: true
+    });
+
+    if (spaceRoomMatchRoute !== false) {
+        return spaceRoomMatchRoute;
+    }
+
+    return matchRoute({
+        to: "/space/$spaceId",
+        fuzzy: true
+    });
+};
+
 export const MainLayout: FC = () => {
+    const spaceRoomMatchRoute = useSpaceRoomMatchRoute();
+
     const style = {
-        "--sidebar-width": "350px"
+        "--sidebar-width":
+            spaceRoomMatchRoute !== false ? "350px" : "calc(var(--sidebar-width-icon)+1px)"
     } as CSSProperties;
 
     return (
         <CallContextProvider>
             <SidebarProvider style={style}>
-                <AppSidebar />
+                <AppSidebar
+                    activeSpaceId={
+                        spaceRoomMatchRoute !== false ? spaceRoomMatchRoute.spaceId : undefined
+                    }
+                    activeRoomId={
+                        spaceRoomMatchRoute !== false ? spaceRoomMatchRoute.roomId : undefined
+                    }
+                />
                 <SidebarInset className="h-dvh overflow-hidden">
                     <Outlet />
                 </SidebarInset>
