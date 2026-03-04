237533e2c0a0ae9f02c6370605080224e16c4471
GRAUX FRANCOIS p2101168 <francois.graux@etu.univ-lyon1.fr>
2026-03-04 21:22:11 +0100

    feat : update style card + the sidebar on presence status change

--- src/components/presence-sidenav.tsx
+++ src/components/presence-sidenav.tsx
@@ -8,12 +8,49 @@ export const PresenceSidenav: FC = () => {
     const presence = usePresence(client);
 
     return (
-        <div className="flex h-full w-80 flex-col gap-2 overflow-y-auto border-l p-3">
-            {Object.entries(presence).map(([userId, data]) => (
-                <div key={userId}>
-                    <ProfileCard displayName={data.displayName} avatarUrl={""} presenceStatus={data.status} />
-                </div>
-            ))}
+        <div className="flex h-full w-80 flex-col gap-4 overflow-y-auto border-l p-3">
+            <div className="flex flex-col gap-2">
+                <h3 className="px-2 text-sm font-semibold text-gray-500">En ligne</h3>
+                {Object.entries(presence)
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    .filter(([_, data]) => data.status !== "offline")
+                    .map(([userId, data]) => (
+                        <div key={userId}>
+                            <ProfileCard
+                                displayName={data.displayName}
+                                avatarUrl={""}
+                                presenceStatus={data.status}
+                            />
+                        </div>
+                    ))}
+            </div>
+
+            {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
+            {Object.entries(presence).filter(([_, data]) => data.status === "offline").length > 0 && (
+                <hr className="border-gray-500" />
+            )}
+
+            <div className="flex flex-col gap-2">
+                {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
+                {Object.entries(presence).filter(([_, data]) => data.status === "offline").length >
+                    0 && (
+                    <>
+                        <h3 className="px-2 text-sm font-semibold text-gray-500">Hors ligne</h3>
+                        {Object.entries(presence)
+                            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                            .filter(([_, data]) => data.status === "offline")
+                            .map(([userId, data]) => (
+                                <div key={userId} className="opacity-60">
+                                    <ProfileCard
+                                        displayName={data.displayName}
+                                        avatarUrl={""}
+                                        presenceStatus={data.status}
+                                    />
+                                </div>
+                            ))}
+                    </>
+                )}
+            </div>
         </div>
     );
 };
--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -24,11 +24,24 @@ export const ProfileCard : FC<Props> = props => {
                         <AvatarImage src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
                     )}
                     <AvatarFallback>ER</AvatarFallback>
-                    <AvatarBadge className="bg-green-600 dark:bg-green-800" />
+                    {props.presenceStatus === "online" && (
+                        <AvatarBadge className="bg-green-500" />
+                    )}
+                    {props.presenceStatus === "unavailable" && (
+                        <AvatarBadge className="bg-red-500" />
+                    )}
+                    {props.presenceStatus === "offline" && (
+                        <AvatarBadge className="bg-gray-500" />
+                    )}
                 </Avatar>
             </ItemMedia>
             <ItemContent>
-                <ItemTitle>{props.displayName}</ItemTitle>
+                {props.presenceStatus === "online" && (
+                    <ItemTitle>{props.displayName}</ItemTitle>
+                )}
+                {props.presenceStatus === "offline" && (
+                    <ItemTitle className="text-gray-500">{props.displayName}</ItemTitle>
+                )}
             </ItemContent>
         </Item>
     );
