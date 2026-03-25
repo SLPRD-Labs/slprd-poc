123ad972696e7cfb839458f799483e0f727a23ef
nina-dry <nina.dorry@gmail.com>
2026-03-25 16:31:26 +0100

    fix return

--- src/components/textual-room/text-chat.tsx
+++ src/components/textual-room/text-chat.tsx
@@ -100,6 +100,7 @@ export const TextChat: FC<Props> = ({ roomId }) => {
         if (e.key === "Enter" && !e.shiftKey) {
             e.preventDefault();
             void sendMain();
+            return;
         }
 
         void client.sendTyping(roomId, true, 4000);
