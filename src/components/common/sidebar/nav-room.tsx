e3eca5fbf9e7c327d6b97a987833c4a24cf17dfe
RUIZ Charlotte <charlotte.ruiz@hotmail.fr>
2026-03-27 16:03:25 +0100

    (create-room) - Suppression import inutile

--- src/components/common/sidebar/nav-room.tsx
+++ src/components/common/sidebar/nav-room.tsx
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
 import { SidebarMenuAction, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import { Link } from "@tanstack/react-router";
-<<<<<<< HEAD
 import { Ellipsis, Hash, Volume2 } from "lucide-react";
-=======
-import { Hash, Volume2, Lock } from "lucide-react";
->>>>>>> 729b139 ((create-room) - Création des room terminée il reste la redirection (Noëllie))
 import type { Room } from "matrix-js-sdk";
 import { RoomEvent } from "matrix-js-sdk";
 import type { FC } from "react";
 import { useEffect, useState } from "react";
-=======
-import { Button } from "@/components/ui/button";
-import { SidebarMenuAction, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
-import { Link } from "@tanstack/react-router";
-import { Ellipsis, Hash, Volume2 } from "lucide-react";
-import { RoomEvent, type Room } from "matrix-js-sdk";
-import { useEffect, useState, type FC } from "react";
->>>>>>> 8a63d35 ((create-room) - Ajout dialog de modification et suppression)
 import { EditRoomDialog } from "../dialogs/edit-room-dialog";
 import { useMatrixClient } from "@/hooks/use-matrix-client";
 
@@ -29,26 +16,15 @@ interface Props {
 }
 
 export const NavRoom: FC<Props> = ({ spaceId, room, isActive, isCall }) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
     const { client } = useMatrixClient();
 
     const [displayName, setDisplayName] = useState(room.name.trim() || room.roomId);
-=======
-    const { client } = useMatrixClient();
-
-    const [displayName, setDisplayName] = useState(room.name?.trim() || room.roomId);
->>>>>>> 8a63d35 ((create-room) - Ajout dialog de modification et suppression)
     const [openEditRoom, setOpenEditRoom] = useState<boolean>(false);
 
     useEffect(() => {
         const onRoomName = (updatedRoom: Room) => {
             if (updatedRoom.roomId === room.roomId) {
-<<<<<<< HEAD
                 setDisplayName(updatedRoom.name.trim() || room.roomId);
-=======
-                setDisplayName(updatedRoom.name?.trim() || room.roomId);
->>>>>>> 8a63d35 ((create-room) - Ajout dialog de modification et suppression)
             }
         };
 
@@ -57,12 +33,6 @@ export const NavRoom: FC<Props> = ({ spaceId, room, isActive, isCall }) => {
             client.off(RoomEvent.Name, onRoomName);
         };
     }, [client, room.roomId]);
-<<<<<<< HEAD
-=======
-    const displayName = room.name?.trim() || room.roomId;
->>>>>>> 729b139 ((create-room) - Création des room terminée il reste la redirection (Noëllie))
-=======
->>>>>>> 8a63d35 ((create-room) - Ajout dialog de modification et suppression)
 
     return (
         <SidebarMenuItem className="flex flex-row items-center justify-between">
@@ -74,18 +44,13 @@ export const NavRoom: FC<Props> = ({ spaceId, room, isActive, isCall }) => {
                         to="/space/$spaceId/room/$roomId"
                         params={{ spaceId: spaceId, roomId: room.roomId }}
                     >
-<<<<<<< HEAD
                         {isCall ? <Volume2 /> : <Hash />}
-=======
-                        {isCall ? <Volume2 /> : <Hash /> } 
->>>>>>> 729b139 ((create-room) - Création des room terminée il reste la redirection (Noëllie))
                         <span>{displayName}</span>
                     </Link>
                 }
             />
             <SidebarMenuAction
                 showOnHover
-<<<<<<< HEAD
                 className="hover:!text-sidebar-foreground hover:!bg-transparent"
                 onClick={() => {
                     setOpenEditRoom(true);
@@ -99,14 +64,6 @@ export const NavRoom: FC<Props> = ({ spaceId, room, isActive, isCall }) => {
                 spaceId={spaceId}
                 room={room}
             />
-=======
-                className="hover:!bg-transparent hover:!text-sidebar-foreground"
-                onClick={() => setOpenEditRoom(true)}
-            >
-                <Ellipsis />
-            </SidebarMenuAction>
-            <EditRoomDialog openEditRoom={openEditRoom} setOpenEditRoom={setOpenEditRoom} spaceId={spaceId} room={room} />    
->>>>>>> 8a63d35 ((create-room) - Ajout dialog de modification et suppression)
         </SidebarMenuItem>
     );
 };
