6543a4bebc18bd109e99b9492520aa126c00ca31
nnmoip <noelliepaulus69@gmail.com>
2026-03-28 17:03:07 +0100

    (join-vocal-channel) - Connexion dans le vocal du salon au clic

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -4,8 +4,12 @@ import { Route } from "@/routes/_mainLayout/space/$spaceId/room/$roomId";
 import { useQuery } from "@tanstack/react-query";
 import type { FC } from "react";
 import { PresenceSidenav } from "@/components/presence-sidenav";
+import { useCallContext } from "@/contexts/call-context/call-context";
+import { Room as CallRoom } from "@/components/room";
 
 export const Room: FC = () => {
+    const call = useCallContext();
+
     const { spaceId, roomId } = Route.useParams();
 
     const { client, ready } = useMatrixClient();
@@ -34,6 +38,18 @@ export const Room: FC = () => {
                 <div className="flex border-b p-3">
                     <h2 className="font-semibold"># {roomQuery.data?.name}</h2>
                 </div>
+                {call.state === "active" && call.room.roomId === roomQuery.data?.roomId && (
+                    <>
+                    <CallRoom liveKitRoom={call.liveKitRoom} />
+                    <button
+                        onClick={() => {
+                            void call.leave();
+                        }}
+                    >
+                        Leave
+                    </button>
+                    </>
+                )}
                 <TextChat roomId={roomId} />
             </div>
             <PresenceSidenav />
